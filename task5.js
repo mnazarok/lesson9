927570d5df35fb6352d749edbe1fac1a6d643b81
andy <mnazarok5@gmail.com>
2025-03-20 22:49:15 +0200

    task5.js

---
+++ task5.js
@@ -0,0 +1,7 @@
+const user = [
+    {name: "Maksym", email: "mnazarok5@gmail.com", age: 27}
+];
+
+for (const {name, email, age} of user) {
+    console.log(`Name is ${name}  Email is ${email} age is ${age}`)
+    }
